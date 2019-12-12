e78effa52a9900684687fde5121be4bf553df3ae
serhat <serhat-sumer@hotmail.com>
2019-12-12 20:24:11 +0100

    feat: create findByKeyValue

--- functions/find-by/key-value/index.js
+++ functions/find-by/key-value/index.js
@@ -3,8 +3,19 @@
 // (later you will learn about req.body to pass any type)
 
 const keyValue = (pokeArray, key, value) => {
-
+  let pokeArr = [];
+  let filteredArr = pokeArray.filter(item => item[key] == value);
+  if (filteredArr.length !== 0) {
+    filteredArr.map(element => {
+      let pokeObj = {};
+      pokeObj.num = element.num;
+      pokeObj.name = element.name;
+      pokeArr.push(pokeObj);
+    });
+    return pokeArr;
+  } else {
+    return pokeArr;
+  }
 };
-
 
 module.exports = keyValue;
