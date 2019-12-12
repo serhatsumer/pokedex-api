df41284c53167f717cebcb77db47a2add6033bf5
serhat <serhat-sumer@hotmail.com>
2019-12-12 20:29:29 +0100

    feat: create findByType

--- functions/find-by/type/index.js
+++ functions/find-by/type/index.js
@@ -1,5 +1,10 @@
 const type = (pokeArray, type) => {
-
+  let filteredArr = pokeArray.filter(item => item.type.includes(type));
+  filteredArr = filteredArr.map(element => ({
+    name: element.name,
+    num: element.num
+  }));
+  return filteredArr;
 };
 
 module.exports = type;
