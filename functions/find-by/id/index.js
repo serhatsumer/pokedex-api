474883f43cab112e633540f1cab8c823d9bc347d
serhat <serhat-sumer@hotmail.com>
2019-12-12 20:08:20 +0100

    feat: create find-by-id

--- functions/find-by/id/index.js
+++ functions/find-by/id/index.js
@@ -1,5 +1,9 @@
 const id = (pokeData, id) => {
-
-}
+  if (pokeData.find(data => data.id === id)) {
+    return pokeData.find(data => data.id === id);
+  } else {
+    return {};
+  }
+};
 
 module.exports = id;
